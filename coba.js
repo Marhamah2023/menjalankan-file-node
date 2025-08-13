3f5bd2b7a511523bf62e6c2c3776788b21da538f
Marhamah2023 <marhamah2002@gmail.com>
2025-08-13 08:10:04 +0700

    mengubah file coba dan index

--- coba.js
+++ coba.js
@@ -6,4 +6,25 @@ function cetakNama(nama) {
 
 // console.log(cetakNama('sandhika'));
 
-module.exports = cetakNama;
+const PI = 3.14;
+
+const mahasiswa = {
+    nama: 'doddy',
+    umur: 20,
+    cetakMhs() {
+        return `halo, nama saya ${this.nama} dan saya ${this.umur} tahun.`;
+    },
+};
+
+class Orang {
+    constructor() {
+        console.log('objek orang telah dibuat');
+    }
+}
+
+// module.exports.cetakNama = cetakNama;
+// module.exports.PI = PI;
+// module.exports.mahasiswa = mahasiswa;
+// module.exports.Orang = Orang;
+
+module.exports = { cetakNama, PI, mahasiswa, Orang };
--- index.js
+++ index.js
@@ -3,8 +3,14 @@
 // const cetakNama = (nama) => `hi, nama saya ${nama}`;
 // console.log(cetakNama('sandhika'));
 
-const cetakNama = require('./coba');
+// const fs = require('fs'); //core module
+// const cetakNama = require('./coba'); //local module
+// const moment = require('moment') // third party module / npm module / node_modules
 
 // console.log('hello wpu');
 
-console.log(cetakNama('sandhika'));
+// const cetakNama = require('./coba');
+// const PI = require('./coba');
+const coba = require('./coba');
+
+console.log(coba.cetakNama('sandhika'), coba.PI, coba.mahasiswa.cetakMhs(), new coba.Orang());
